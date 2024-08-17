1bc573387febe98e8c5953910e059027282e3ca7
Lett <lett@ubuntu-23.10-desktop>
2024-08-17 17:36:29 -0400

    events + pin asked for

--- main.py
+++ main.py
@@ -2,18 +2,35 @@
 # import Light
 import RPi.GPIO as GPIO
 
-GPIO_PIN: int = 16
+
+def get_pin_number() -> int:
+    """
+    Gets the pin number from the user's input.
+    :return: integer representing the pin number
+    """
+    try:
+        pin_number = input("Type in the pin that the button uses:\n> ")
+        int(pin_number)
+    except ValueError:
+        print("The value typed in was not able to be converted into an integer. Ensure that you've typed it correctly.")
+        get_pin_number()
 
 
-def main():
+def button_callback():
+    print("Button was pushed!")
+
+
+def main(pin_number: int):
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    while True:
-        if GPIO.input(GPIO_PIN) == GPIO.HIGH:
-            print("Button was pushed!")
+    GPIO.add_event_detect(pin_number, GPIO.RISING, callback=button_callback())
+
+    input("Press enter to kill the program.")
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
-    main()
+    GPIO_PIN_NUMBER: int = get_pin_number()
+    main(GPIO_PIN_NUMBER)
